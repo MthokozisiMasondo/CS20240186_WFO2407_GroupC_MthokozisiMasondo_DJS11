4651e31afa2529f7d549162bbf019c43cfba581f
Mthokozisi Masondo <m.masondo2001@gmail.com>
2024-11-29 16:01:27 +0200

    added vite config for deployment

--- podcast-app/src/components/Header.jsx
+++ podcast-app/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Logo from "../assets/logo.png"
-import SearchIcon from "../assets/search.png"
 import Favourites from "../assets/favourites.png" 
 
 // Header container
